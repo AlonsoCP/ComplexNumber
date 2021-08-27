0cb3704dd7b3e088b0d07d228ac777c71588f187
Alonso Cabezut <a2131601@correo.uia.mx>
2021-08-26 19:39:00 -0500

    changes to header and test

--- libs/ComplexNumber.h
+++ libs/ComplexNumber.h
@@ -1,14 +1,15 @@
 /**
- * @Author Alonso Cabezut Portilla
- * @Date   17/Aug/2021
- * @Proyect ComplexNumber.h
+ * @Author Cesarin Anggelidis
+ * @Date 17 Aug 2021
+ * @Name ComplexNumber.h
  * @Copyright TDA Otoño 2021
- **/
+ */
+
+// SOLID Convention 
 
 #ifndef ComplexNumber_h
 #define ComplexNumber_h
-
-// Standard Libraries
+// Bibliotecas Standard
 #include <stdio.h>
 
 // User Libraries
@@ -17,25 +18,31 @@
 // Data Types
 
 struct _Complex_Number{
-    float real;
-    float imaginary;
-}
+    float real; // 32 bits
+    float imaginary; // 32 bit
+};
 
 // Wrapper
-typedef struct _Complex_Number * ComplexNumber;
+typedef struct _Complex_Number* ComplexNumber;
+typedef enum{
+    _FALSE,
+    _TRUE
+}_bool;
 
-
-//Interfaces
-
+// Interfaces
 
 // Generator/Constructors
 
-ComplexNumber newComplexNumber(float real, float imaginary);
+
+ComplexNumber newComplexNumber(void);
 
 
+ComplexNumber setComplexNumber(ComplexNumber phasor, float real, float imaginary);
 
 
+ComplexNumber getComplexNumberComponents(ComplexNumber phasor, float *real, float *imaginary);
 
 
+ComplexNumber freeComplexNumber(ComplexNumber phasor);
 
 #endif
